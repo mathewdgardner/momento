1755fe03343d43abeee399e90f1db1a9754bad3a
Mathew Gardner <mathew@vin.li>
2016-07-01 14:52:43 -0500

    Add quick and dirty ISO8601 and ISO date parsing.

--- lib/date.ex
+++ lib/date.ex
@@ -5,26 +5,51 @@ defmodule Momento.Date do
   # date/1
   def date(%DateTime{} = arg), do: arg
 
-  # TODO: Add timezone support first
-  # def date(arg) when is_bitstring(arg) do
-  #   cond do
-  #     # ISO8601 - "2016-04-20T15:05:13.991Z"
-  #     Regex.match?(~r/^[0-9]{4}-[0-9]{2}-[0-9]{2}[tT\s][0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}[zZ]$/, arg) ->
-  #       [date, time] = String.split(arg, "T")
-  #       [year, month, day] = String.split(date, "-")
-  #       [hour, minute, seconds] = String.split(time, ":")
-  #       [second, milliseconds] = String.split(seconds, ".")
-  #       [millisecond, _] = String.split(milliseconds, "Z")
-  #       %DateTime{year: year, month: month, day: day, hour: hour, minute: minute, second: second, microsecond: {millisecond * 1000, 6}}
-  #
-  #     # ISO date - "2016-04-20"
-  #     Regex.match?(~r/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/, arg) ->
-  #       [year, month, day] = String.split(date, "-")
-  #       %DateTime{year: year, month: month, day: day, hour: 0, minute: 0, second: 0, microsecond: {0, 6}}
-  #
-  #     true -> {:error, "Unknown date format."}
-  #   end
-  # end
+  # TODO: Add timezone support
+  def date(arg) when is_bitstring(arg) do
+    cond do
+      # ISO8601 - "2016-04-20T15:05:13.991Z"
+      Regex.match?(~r/^[0-9]{4}-[0-9]{2}-[0-9]{2}[tT\s][0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}[zZ]$/, arg) ->
+        [date, time] = String.split(arg, "T")
+        [year, month, day] = String.split(date, "-")
+        [hour, minute, seconds] = String.split(time, ":")
+        [second, milliseconds] = String.split(seconds, ".")
+        [millisecond, _] = String.split(milliseconds, "Z")
+
+        %DateTime{
+          year: String.to_integer(year),
+          month: String.to_integer(month),
+          day: String.to_integer(day),
+          hour: String.to_integer(hour),
+          minute: String.to_integer(minute),
+          second: String.to_integer(second),
+          microsecond: {String.to_integer(millisecond) * 1000, 6},
+          std_offset: 0,
+          utc_offset: 0,
+          time_zone: "Etc/UTC",
+          zone_abbr: "UTC"
+        }
+
+      # ISO date - "2016-04-20"
+      Regex.match?(~r/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/, arg) ->
+        [year, month, day] = String.split(arg, "-")
+        %DateTime{
+          year: String.to_integer(year),
+          month: String.to_integer(month),
+          day: String.to_integer(day),
+          hour: 0,
+          minute: 0,
+          second: 0,
+          microsecond: {0, 6},
+          std_offset: 0,
+          utc_offset: 0,
+          time_zone: "Etc/UTC",
+          zone_abbr: "UTC"
+        }
+
+      true -> {:error, "Unknown date format."}
+    end
+  end
 
   # Unix epoch
   def date(s) when is_integer(s) and s > 999999999999999999, do: DateTime.from_unix(s, :nanoseconds)
--- spec/date_spec.exs
+++ spec/date_spec.exs
@@ -27,7 +27,27 @@ defmodule DateSpec do
     end
 
     describe "ISO8601" do
-      it "should return a DateTime struct from a ISO8601 string"
+      it "should return a DateTime struct from a ISO8601 string" do
+        datetime = Momento.date("2016-04-20T15:05:13.991Z")
+
+        expect(datetime.year) |> to(eql 2016)
+        expect(datetime.month) |> to(eql 4)
+        expect(datetime.day) |> to(eql 20)
+        expect(datetime.hour) |> to(eql 15)
+        expect(datetime.minute) |> to(eql 5)
+        expect(datetime.second) |> to(eql 13)
+        expect(datetime.microsecond) |> to(eql {991000, 6})
+      end
+    end
+
+    describe "ISO date" do
+      it "should return a DateTime struct from a ISO date string" do
+        datetime = Momento.date("2016-04-20")
+
+        expect(datetime.year) |> to(eql 2016)
+        expect(datetime.month) |> to(eql 4)
+        expect(datetime.day) |> to(eql 20)
+      end
     end
   end
 
